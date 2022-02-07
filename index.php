f418af4c9a312786bb16fe057c113ce44101c6c9
hossainRana <rana.cseewu@gmail.com>
2022-02-07 17:29:43 +0600

    initial

---
+++ index.php
@@ -0,0 +1,284 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PT Trim Fat Burn™ (Official) | Get $1782 Off Today! Buy PT Trim</title>
+    <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+
+    <link rel="stylesheet" href="./css/main.css">
+
+    <!--            Bootstrap CSS           -->
+
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap-grid.rtl.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap-reboot.rtl.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap-reboot.rtl.min.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+
+
+    <!--            Bootstrap Js            -->
+
+    <script src="./bootstrap/js/bootstrap.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="./bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.esm.js"></script>
+    <script src="./bootstrap/js/bootstrap.esm.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+    <nav class="top-nav" id="top-nav">
+        <a href="./" class="logo">
+            <span>PT Trim Fat Burn™</span>
+        </a>
+    </nav>
+
+    <div class="banner-section">
+        <div class="image-container">
+            <img src="./images/banner.jpg" alt="ptm-banner--image">
+        </div>
+        <div style="width: 100%; height: 90vh; backdrop-filter: brightness(0.5); position: absolute; top: 0;">
+            <h1 class="banner-text">
+                GIVING YOU THE BEST<br/> COMMISSION ON ALL OUR FUNNELS
+            </h1>
+        </div>
+        <div class="scroll-down">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+<!--    <div class="trim-affiliate d-flex justify-content-center">
+        <ul>
+            <li>$2+ EPC’s with Best Commission Tiers</li>
+            <li>$10,000 Plus Potential Per Single Mailing</li>
+            <li>High Conversions with Both Warm & Cold Traffic</li>
+            <li>Over $582 Per Sale Potential</li>
+        </ul>
+    </div>-->
+
+    <div class="product-container">
+        <div class="container">
+            <div class="heading-text text-center">
+                <p>GIVING YOU THE BEST COMMISSION ON ALL OUR FUNNELS</p>
+                <span>Learn More NOW Below!</span>
+            </div>
+            <div class="row">
+                <p class="commission-text">We're paying the best commissions on all three of our best products and you stand to earn up to $582 in commissions per single sale. You will get the best commissions through the entire product lineup below.*</p>
+                <div class="col-lg-6 mt-3">
+                    <div class="each-product--card">
+                        <div class="product-details">
+                            <div class="product-image--container">
+                                <div class="side-text">
+                                    <p><b>From :</b> Hot Water</p>
+                                    <p><b>From 2:</b> Fat-burning breakfast</p>
+                                    <p><b>Subject:</b> NEW “purple tea” melts over 100lbs</p>
+                                    <p><b>Subject 2:</b> NEW “purple tea” melts 2lbs daily</p>
+                                </div>
+                                <img src="./images/pttrim-aff.png" alt="product-images">
+                            </div>
+                            <p>
+                                I’m sure you’ve been told that drinking tea in the morning can help you lose weight, however…
+                                That’s completely not true.
+                                I know people who drink tons of green and black tea yet their weight NEVER budges.
+                                The good news is…
+                                There’s a new type of “purple tea” you can have right at home…
+                                That melts away stubborn belly fat like crazy.
+                                My friend Liz used it to drop over 100 pounds when Weight Watchers and keto failed her.
+                            </p>
+                            <p class="light-text">NEW “Purple Tea” Melts Away Over 100lbs</p>
+                            <i>PS - Here she is down 100lbs and 10 jean sizes in a matter of months, so be ready because...
+                                It works really fast :)</i>
+                            <a class="buy-now--btn" href="https://pttrimfatburn.com/?hop=abhipapa" target="_blank">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt-3">
+                    <div class="each-product--card">
+                        <div class="product-details">
+                            <div class="product-image--container">
+                                <img src="./images/pttrim-aff.png" alt="product-images">
+                                <div class="side-text">
+                                    <p><b>From :</b> Lemons</p>
+                                    <p><b>From 2:</b> ACV</p>
+                                    <p><b>Subject:</b> Mix In Warm Water To Drop 50lbs</p>
+                                </div>
+                            </div>
+                            <b>If you want to drop 15, 25, even 50lbs or more, you could:</b>
+                            <ol>
+                                <li>
+                                    Fill your plate with tons of veggies and dry chicken breast at every single meal
+                                </li>
+                                or
+                                <li>
+                                    Mix this in warm water after you wake up.
+                                </li>
+                            </ol>
+                            <p>
+                                The reason this works so well is because it contains new compounds called catechins…
+                                Which forces your body to double or even triple the amount of calories you burn throughout the day…
+                                Even if you don’t exercise.
+                                The science behind this is still pretty new and I don’t want to bore you with it, just know...
+                                If you want a flatter belly starting tomorrow…
+                            </p>
+                            <p class="light-text">Mix in Warm Water to Drop 50lbs</p>
+                            <a class="buy-now--btn" href="https://pttrimfatburn.com/?hop=abhipapa" target="_blank">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt-3">
+                    <div class="each-product--card">
+                        <div class="product-details">
+                            <div class="product-image--container">
+                                <div class="side-text">
+                                    <p><b>From :</b> Diet hack</p>
+                                    <p><b>Subject:</b> Banana oatmeal</p>
+                                </div>
+                                <img src="./images/pttrim-aff.png" alt="product-images">
+                            </div>
+                            <p>
+                                There’s a NEW way to burn off a lot of belly fat, and…
+                                It has nothing to do with meal plans, cardio or counting calories.
+                                It’s based on a recent discovery that targets belly fat just by adding one new thing to your diet.
+                                You can have it with breakfast, lunch or dinner…
+                                Just make sure you have it at least once a day if you have 20lbs or more to lose.
+                            </p>
+                            <p class="light-text">Do This Before Breakfast To Melt Away 20lbs of Dangerous Belly Fat</p>
+                            <i>PS - My friend Liz did it and lost over 100 pounds in a matter of months, so…
+                                Just be ready for the extra fat to come off real fast.</i>
+                            <p class="light-text">Do This for Breakfast To Melt Belly Fat Like Crazy</p>
+                            <a class="buy-now--btn" href="https://pttrimfatburn.com/?hop=abhipapa" target="_blank">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt-3">
+                    <div class="each-product--card">
+                        <div class="product-details">
+                            <div class="product-image--container">
+                                <img src="./images/pttrim-aff.png" alt="product-images">
+                                <div class="side-text">
+                                    <p><b>From :</b> 14lbs in 7 Days</p>
+                                    <p><b>From 2:</b> New Morning Ritual</p>
+                                    <p><b>Subject:</b> Purple tea trick melts 2lbs daily</p>
+                                </div>
+                            </div>
+                            <p>
+                                I’m sure you’ve been told to start drinking green tea if you want to drop a few pounds, however…
+                                It just doesn’t work.
+                                You have to drink like 10 cups a day just to notice a tiny difference.
+                                The good news is…
+                                There’s a new type of “purple tea” that burns fat like crazy and melts up to 2lbs a day.
+                            </p>
+                            <b>Here it is:</b>
+                            <p class="light-text">Purple Tea Trick Melts Up To 2lbs Daily</p>
+                            <p>
+                                Over 22,877 people have tried it so far with some amazing results…
+                                Like dropping 2 jean sizes in the first week, so…
+                                Give it a shot now before it becomes too mainstream.
+                            </p>
+                            <p class="light-text">Purple Tea Trick Melts Up To 2lbs Daily</p>
+                            <a class="buy-now--btn" href="https://pttrimfatburn.com/?hop=abhipapa" target="_blank">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt-3">
+                    <div class="each-product--card">
+                        <div class="product-details">
+                            <div class="product-image--container">
+                                <div class="side-text">
+                                    <p><b>From :</b> Chamomile tea</p>
+                                    <p><b>From2 :</b> Pajamas</p>
+                                    <p><b>Subject:</b> Bedtime ritual torches belly fat</p>
+                                </div>
+                                <img src="./images/pttrim-aff.png" alt="product-images">
+                            </div>
+                            <p>
+                                Every night after she puts her kids to bed…
+                                Liz sits in her rocking chair and sips a warm drink.
+                                She needs the extra energy, after all…
+                                She’s raising three kids on her own…
+                                She always puts their needs before hers…
+                                And before she knew it…
+                                She stepped on the scale and saw that horrifying number staring back at her…
+                                290lbs.
+                                That was the moment things HAD to change.
+                                So she started making this purple drink…
+                                That a friend shared with her...
+                                Because it’s packed with catechins.
+                                Catechins are a rare plant compound that specifically target the “jiggly” fat around your belly, hips and thighs.
+                                Not only is her belly completely gone…
+                                She lost over 100 pounds and 10 jean sizes.
+                            </p>
+                            <p class="light-text">NEW Bedtime Ritual Melts Belly Fat Like Crazy</p>
+                            <a class="buy-now--btn" href="https://pttrimfatburn.com/?hop=abhipapa" target="_blank">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt-3">
+                    <div class="each-product--card">
+                        <div class="product-details">
+                            <div class="product-image--container">
+                                <img src="./images/pttrim-aff.png" alt="product-images">
+                                <div class="side-text">
+                                    <p><b>From :</b> Heart Clogging Fat</p>
+                                    <p><b>From 2:</b> Bacon Heart Attack</p>
+                                    <p><b>Subject:</b> Why keto doesn’t work</p>
+                                </div>
+                            </div>
+                            <p>
+                                If you have 25 or more pounds to lose…
+                                You could cut out all the bread…
+                                All the carbs…
+                                And all the sweets forever, or....
+                                You could make yourself a powerful fat-burning drink before you go to bed at night.
+                                I know which one sounds better to me :)
+                                Look, keto does work and it can be really healthy for you…
+                                But let’s be real…
+                                You know yourself better than anyone…
+                                So if you think you can cut out all the bread, carbs and sweets for the rest of your life…
+                                Then by all means, do it.
+                                However, if that sounds way too hard…
+                                Then don’t force yourself to do something that doesn’t fit your lifestyle.
+                                Instead, just make sure to <span class="light-text">get these specific fat-burning nutrients</span> before you go to bed…
+                                Because they trick your body into burning more calories at night, and in turn…
+                                More belly fat.
+                            </p>
+                            <p class="light-text">25lbs in Just 25 Days Without Changing Your Diet</p>
+                            <p>
+                                Luckily, there’s more than one way to get rid of your belly fat, so…
+                                If strict dieting and daily exercise just doesn’t work for you…
+                                Keep trying new things until you find what works for YOUR body.
+                            </p>
+                            <a class="buy-now--btn" href="https://pttrimfatburn.com/?hop=abhipapa" target="_blank">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<script>
+    let lastScrollpos = window.pageYOffset;
+    let menuButtonCheck = document.querySelector(".menu-btn");
+    window.onscroll = function() {
+        let currentScrollPos = window.pageYOffset;
+        if (lastScrollpos > currentScrollPos) {
+            document.getElementById("top-nav").style.top = "0";
+        } else {
+            document.getElementById("top-nav").style.top = "-75px";
+        }
+        lastScrollpos = currentScrollPos;
+        menuButtonCheck.checked = false;
+    }
+</script>
